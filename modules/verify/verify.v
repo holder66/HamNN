3620a95b4543c834858792f256311eca788b3a4e
Henry Olders <henry@olders.ca>
2021-07-31 22:06:11 -0400

    moved show expanded verify result to a separate file, tools/show.v

--- modules/verify/verify.v
+++ modules/verify/verify.v
@@ -41,20 +41,7 @@ pub fn verify(cl tools.Classifier, opts tools.Options) tools.VerifyResult {
 		println('correct inferences: $verify_result.correct_count out of $verify_result.labeled_classes.len (${percent:5.2f}%)')
 	}
 	if opts.expanded_flag && opts.command == 'verify' {
-		mut show_expanded_verify_result := ['', 'Class                          Cases in         Correctly        Incorrectly  Wrongly classified', '                               test set          inferred           inferred     into this class']
-		mut total_cases := 0 
-		mut total_correct := 0 
-		mut total_incorrect := 0 
-		mut total_wrong := 0
-		for class, value in verify_result.class_table {
-			show_expanded_verify_result << '${class:-27}       ${value.labeled_instances:5}   ${value.correct_inferences:5} (${f32(value.correct_inferences) * 100 / value.labeled_instances:6.2f}%)    ${value.missed_inferences:5} (${f32(value.missed_inferences) * 100 / value.labeled_instances:6.2f}%)     ${value.wrong_inferences:5} (${f32(value.wrong_inferences) * 100 / value.labeled_instances:6.2f}%)'
-			total_cases += value.labeled_instances
-			total_correct += value.correct_inferences
-			total_incorrect += value.missed_inferences
-			total_wrong += value.wrong_inferences
-		}
-		show_expanded_verify_result << '\nTotals                            ${total_cases:5}   ${total_correct:5} (${f32(total_correct) * 100 / total_cases:6.2f}%)    ${total_incorrect:5} (${f32(total_incorrect) * 100 / total_cases:6.2f}%)     ${total_wrong:5} (${f32(total_wrong) * 100 / total_cases:6.2f}%)'
-		tools.print_array(show_expanded_verify_result)
+		tools.show_expanded_result(verify_result)
 	}
 	return verify_result
 }
