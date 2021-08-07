eae45994b08e237e74c5a041a193c6e74f2e8f15
Henry Olders <henry@olders.ca>
2021-08-07 15:43:18 -0400

    added opts.uniform_bins to explore_test.v

--- modules/explore/explore_test.v
+++ modules/explore/explore_test.v
@@ -12,6 +12,7 @@ fn test_explore_cross() {
 		bins: [2, 12]
 		show_flag: false
 		concurrency_flag: true
+		uniform_bins: true
 		datafile_path: 'datasets/iris.tab'
 	}
 	mut ds := tools.load_file(opts.datafile_path)
@@ -21,16 +22,31 @@ fn test_explore_cross() {
 	assert results[0].wrong_count == 50
 	assert results[0].total_count == 150
 
+	opts.uniform_bins = false
+	results = explore(ds, opts)
+	assert results[results.len - 1].correct_count == 141
+	assert results[results.len - 1].misses_count == 9
+	assert results[results.len - 1].wrong_count == 9
+	assert results[results.len - 1].total_count == 150
+
 	opts.folds = 10
 	opts.number_of_attributes = [27,29]
 	opts.bins = [20,22]
 	opts.weighting_flag = true
 	opts.datafile_path = 'datasets/anneal.tab'
+	opts.uniform_bins = true
 	ds = tools.load_file(opts.datafile_path)
 	results = explore(ds, opts)
 	assert results[1].correct_count == 875
 	assert results[1].misses_count == 23
 	assert results[1].wrong_count == 23
+	assert results[1].total_count == 898
+
+	opts.uniform_bins = false
+	results = explore(ds, opts)
+	assert results[1].correct_count == 878
+	assert results[1].misses_count == 20
+	assert results[1].wrong_count == 20
 	assert results[1].total_count == 898
 }
 
