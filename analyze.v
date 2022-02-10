87441d31c438c85e57b3ae2e9eeac1812ea3e578
Henry Olders <henry@olders.ca>
2022-02-10 17:05:06 -0500

    attributes for training tables working

--- analyze.v
+++ analyze.v
@@ -3,9 +3,8 @@ module hamnn
 
 import math
 
-// analyze_dataset returns an array of strings with information about a
-// datafile; print to the console with print_array().
-// ```sh
+// analyze_dataset returns a struct with information about a datafile.
+// Optional: if show_flag is true, print to the console (using show_analyze):
 // 1. a list of attributes, their types, the unique values, and a count of
 // missing values;
 // 2. a table with counts for each type of attribute;
@@ -44,84 +43,6 @@ pub fn analyze_dataset(ds Dataset, opts Options) AnalyzeResult {
 	return result
 }
 
-pub // show_analyze 
-fn show_analyze(result AnalyzeResult) {
-	// println(result)
-	mut show := []string{}
-	show << [
-	'',
-	'Analysis of Dataset "$result.datafile_path" (File Type $result.datafile_type)',
-	'All Attributes', 'Index  Name                          Count  Uniques  Missing      %  Type',
-	'_____  __________________________  _______  _______  _______  _____  ____'
-	]
-	for attr in result.attributes {
-		show << '${attr.id:5}  ${attr.name:-27}  ${attr.count:6}  ${attr.uniques:7}  ${attr.missing:7}  ${attr.missing * 100 / f32(attr.count):5.1f}  ${attr.att_type:4}'
-		}
-	mut total_count := 0
-	mut total_missings := 0
-	for attr in result.attributes { 
-		total_count += attr.count
-		total_missings += attr.missing
-	}
-	show << [
-	'______                             _______           _______  _____',
-	'Totals (less Class attribute)   ${total_count:10}        ${total_missings:10}  ${total_missings * 100 / f32(total_count):5.2f}%'
-	]
-	print_array(show)
-}
-
-pub fn analyze_dataset_old(ds Dataset) []string {
-	cases_count := ds.data[0].len
-	mut show_dataset := ['']
-	mut missing_vals := ds.data.map(missing_values(it))
-	missing_vals << 0
-	mut show_attributes := ['', 'Analysis of Dataset "$ds.path" (File Type ${file_type(ds.path)})',
-		'All Attributes', 'Index  Name                          Count  Uniques  Missing     %   Type',
-		'_____  __________________________  _______  _______  _______  ____   ____']
-	for i, name in ds.attribute_names {
-		show_attributes << '${i:5}  ${name:-27}   ${ds.data[i].len:5}    ${uniques(ds.data[i]):5}     ${missing_vals[i]:4} ${f32(missing_vals[i]) / f32(cases_count) * 100.0:5.1f}   ${ds.inferred_attribute_types[i]}'
-	}
-	show_attributes << '______                             _______           _______ _____'
-	show_attributes << 'Totals (less Class attribute)   ${cases_count * (ds.data.len - 1):10}        ${array_sum<int>(missing_vals):10}  ${f32(array_sum<int>(missing_vals)) / (cases_count * (ds.data.len - 1)) * 100.0:5.2f}%'
-	mut show_types := ['', 'Counts of Attributes by Type', 'Type        Count', '____        _____']
-	for key, value in string_element_counts(ds.inferred_attribute_types) {
-		show_types << '$key          ${value:6}'
-	}
-	show_types << 'Total:     ${ds.inferred_attribute_types.len:6}'
-
-	mut show_discrete_attributes := ['', 'Discrete Attributes for Training',
-		' Index  Name                           Uniques',
-		' _____  __________________________     _______']
-	for key, value in ds.useful_discrete_attributes {
-		show_discrete_attributes << '${key:6}  ${ds.attribute_names[key]:-27}      ${uniques(value):5}'
-	}
-
-	mut show_continuous_attributes := ['', 'Continuous Attributes for Training',
-		' Index  Name                           Min         Max',
-		' _____  __________________________  ______      ______']
-	mut min := 0.0
-	for key, value in ds.useful_continuous_attributes {
-		// to calculate the minimum, strip out missing values (placeholder is -math.max_f32)
-		min = f32_abs(array_min(value.filter(it != -math.max_f32)))
-
-		show_continuous_attributes << '${key:6}  ${ds.attribute_names[key]:-27} ${min:6.3g}      ${array_max(value):6}'
-	}
-
-	mut show_class := ['', 'The Class Attribute: "$ds.Class.class_name"',
-		'Class Value           Cases', '____________________  _____']
-	for key, value in ds.Class.class_counts {
-		show_class << '${key:-20}  ${value:5}'
-	}
-
-	show_dataset << show_attributes
-	show_dataset << show_types
-	show_dataset << show_discrete_attributes
-	show_dataset << show_continuous_attributes
-	show_dataset << show_class
-
-	return show_dataset
-}
-
 // uniques
 fn uniques(attribute_values []string) int {
 	return string_element_counts(attribute_values).len
@@ -132,6 +53,3 @@ fn missing_values(attribute_values []string) int {
 	return attribute_values.filter(it in missings).len
 }
 
-// sum_elements
-fn sum_elements() ? {
-}
--- show.v
+++ show.v
@@ -3,72 +3,135 @@
 module hamnn
 
 import etienne_napoleone.chalk
-// import math
+import math
 
 // show
 pub fn show<T>(a []T) string {
 	return typeof(a).name
 }
 
-// show_analyze prints to the console, information regarding a dataset:
-// ```sh
-// 1. a list of attributes, their types, the unique values, and a count of
-// missing values;
-// 2. a table with counts for each type of attribute;
-// 3. a list of discrete attributes useful for training a classifier;
-// 4. a list of continuous attributes useful for training a classifier;
-// 5. a breakdown of the class attribute, showing counts for each class.
-// ```
-// pub fn show_analyze(ds Dataset) {
-// 	cases_count := ds.data[0].len
-// 	mut show_dataset := ['']
-// 	mut missing_vals := ds.data.map(missing_values(it))
-// 	missing_vals << 0
-// 	mut show_attributes := ['', 'Analysis of Dataset "$ds.path" (File Type ${file_type(ds.path)})',
-// 		'All Attributes', 'Index  Name                          Count  Uniques  Missing     %   Type',
-// 		'_____  __________________________  _______  _______  _______  ____   ____']
-// 	for i, name in ds.attribute_names {
-// 		show_attributes << '${i:5}  ${name:-27}   ${ds.data[i].len:5}    ${uniques(ds.data[i]):5}     ${missing_vals[i]:4} ${f32(missing_vals[i]) / f32(cases_count) * 100.0:5.1f}   ${ds.inferred_attribute_types[i]}'
-// 	}
-// 	show_attributes << '______                             _______           _______ _____'
-// 	show_attributes << 'Totals (less Class attribute)   ${cases_count * (ds.data.len - 1):10}        ${array_sum<int>(missing_vals):10}  ${f32(array_sum<int>(missing_vals)) / (cases_count * (ds.data.len - 1)) * 100.0:5.2f}%'
-// 	mut show_types := ['', 'Counts of Attributes by Type', 'Type        Count', '____        _____']
-// 	for key, value in string_element_counts(ds.inferred_attribute_types) {
-// 		show_types << '$key          ${value:6}'
-// 	}
-// 	show_types << 'Total:     ${ds.inferred_attribute_types.len:6}'
+pub // show_analyze 
+fn show_analyze(result AnalyzeResult) {
+	// println(result)
+	mut show := []string{}
+	show << [
+	'',
+	'Analysis of Dataset "$result.datafile_path" (File Type $result.datafile_type)',
+	'All Attributes', 'Index  Name                          Count  Uniques  Missing      %  Type',
+	'_____  __________________________  _______  _______  _______  _____  ____'
+	]
+	for attr in result.attributes {
+		show << '${attr.id:5}  ${attr.name:-27}  ${attr.count:6}  ${attr.uniques:7}  ${attr.missing:7}  ${attr.missing * 100 / f32(attr.count):5.1f}  ${attr.att_type:4}'
+		}
+	mut total_count := 0
+	mut total_missings := 0
+	for attr in result.attributes { 
+		total_count += attr.count
+		total_missings += attr.missing
+	}
+	show << [
+	'______                             _______           _______  _____',
+	'Totals (less Class attribute)   ${total_count:10}        ${total_missings:10}  ${total_missings * 100 / f32(total_count):5.2f}%'
+	]
+	show << [
+	'', 
+	'Counts of Attributes by Type', 
+	'Type        Count', '____        _____'
+	]
+	mut types := []string{}
+	for attr in result.attributes {
+		types << attr.att_type
+	}
+	for key, value in string_element_counts(types) {
+		show << '$key          ${value:6}'
+	}
+	show << 'Total:     ${types.len:6}'
+	show << [
+	'', 
+	'Discrete Attributes for Training',
+	' Index  Name                        Uniques',
+	' _____  __________________________  _______'
+	]
+	for attr in result.attributes.filter(it.for_training && it.att_type == 'D') {
+		show << '${attr.id:6}  ${attr.name:-27} ${attr.uniques:7}'
+	}
+	show << [
+	'', 
+	'Continuous Attributes for Training',	
+	' Index  Name                               Min         Max',
+	' _____  __________________________  __________  __________'
+	]
+	for attr in result.attributes.filter(it.for_training && it.att_type == 'C') {
+		show << '${attr.id:6}  ${attr.name:-27} ${attr.min:10.3g}  ${attr.max:10.3g}'
+	}
+	mut class_name := ''
+	for attr in result.attributes.filter(it.att_type == 'c') {
+		class_name = attr.name
+	}
+	show << [
+	'', 
+	'The Class Attribute: "$class_name"',
+	'Class Value           Cases', 
+	'____________________  _____'
+	]
+	for attr in result.attributes.filter(it.att_type == 'c') {
+		show << '$attr.name'
+	}
+	print_array(show)
+}
 
-// 	mut show_discrete_attributes := ['', 'Discrete Attributes for Training',
-// 		' Index  Name                           Uniques',
-// 		' _____  __________________________     _______']
-// 	for key, value in ds.useful_discrete_attributes {
-// 		show_discrete_attributes << '${key:6}  ${ds.attribute_names[key]:-27}      ${uniques(value):5}'
-// 	}
+pub fn analyze_dataset_old(ds Dataset) []string {
+	cases_count := ds.data[0].len
+	mut show_dataset := ['']
+	mut missing_vals := ds.data.map(missing_values(it))
+	missing_vals << 0
+	mut show_attributes := ['', 'Analysis of Dataset "$ds.path" (File Type ${file_type(ds.path)})',
+		'All Attributes', 'Index  Name                          Count  Uniques  Missing     %   Type',
+		'_____  __________________________  _______  _______  _______  ____   ____']
+	for i, name in ds.attribute_names {
+		show_attributes << '${i:5}  ${name:-27}   ${ds.data[i].len:5}    ${uniques(ds.data[i]):5}     ${missing_vals[i]:4} ${f32(missing_vals[i]) / f32(cases_count) * 100.0:5.1f}   ${ds.inferred_attribute_types[i]}'
+	}
+	show_attributes << '______                             _______           _______ _____'
+	show_attributes << 'Totals (less Class attribute)   ${cases_count * (ds.data.len - 1):10}        ${array_sum<int>(missing_vals):10}  ${f32(array_sum<int>(missing_vals)) / (cases_count * (ds.data.len - 1)) * 100.0:5.2f}%'
+	mut show_types := ['', 'Counts of Attributes by Type', 'Type        Count', '____        _____']
+	for key, value in string_element_counts(ds.inferred_attribute_types) {
+		show_types << '$key          ${value:6}'
+	}
+	show_types << 'Total:     ${ds.inferred_attribute_types.len:6}'
 
-// 	mut show_continuous_attributes := ['', 'Continuous Attributes for Training',
-// 		' Index  Name                           Min         Max',
-// 		' _____  __________________________  ______      ______']
-// 	mut min := 0.0
-// 	for key, value in ds.useful_continuous_attributes {
-// 		// to calculate the minimum, strip out missing values (placeholder is -math.max_f32)
-// 		min = f32_abs(array_min(value.filter(it != -math.max_f32)))
+	mut show_discrete_attributes := ['', 'Discrete Attributes for Training',
+		' Index  Name                           Uniques',
+		' _____  __________________________     _______']
+	for key, value in ds.useful_discrete_attributes {
+		show_discrete_attributes << '${key:6}  ${ds.attribute_names[key]:-27}      ${uniques(value):5}'
+	}
 
-// 		show_continuous_attributes << '${key:6}  ${ds.attribute_names[key]:-27} ${min:6.3g}      ${array_max(value):6}'
-// 	}
+	mut show_continuous_attributes := ['', 'Continuous Attributes for Training',
+		' Index  Name                           Min         Max',
+		' _____  __________________________  ______      ______']
+	mut min := 0.0
+	for key, value in ds.useful_continuous_attributes {
+		// to calculate the minimum, strip out missing values (placeholder is -math.max_f32)
+		min = f32_abs(array_min(value.filter(it != -math.max_f32)))
 
-// 	mut show_class := ['', 'The Class Attribute: "$ds.Class.class_name"',
-// 		'Class Value           Cases', '____________________  _____']
-// 	for key, value in ds.Class.class_counts {
-// 		show_class << '${key:-20}  ${value:5}'
-// 	}
+		show_continuous_attributes << '${key:6}  ${ds.attribute_names[key]:-27} ${min:6.3g}      ${array_max(value):6}'
+	}
 
-// 	show_dataset << show_attributes
-// 	show_dataset << show_types
-// 	show_dataset << show_discrete_attributes
-// 	show_dataset << show_continuous_attributes
-// 	show_dataset << show_class
-// 	print_array(show_dataset)
-// }
+	mut show_class := ['', 'The Class Attribute: "$ds.Class.class_name"',
+		'Class Value           Cases', '____________________  _____']
+	for key, value in ds.Class.class_counts {
+		show_class << '${key:-20}  ${value:5}'
+	}
+
+	show_dataset << show_attributes
+	show_dataset << show_types
+	show_dataset << show_discrete_attributes
+	show_dataset << show_continuous_attributes
+	show_dataset << show_class
+
+	return show_dataset
+}
+
 
 // show_classifier outputs to the console information about a classifier
 pub fn show_classifier(cl Classifier) {
@@ -92,7 +155,7 @@ pub fn show_classifier(cl Classifier) {
 	println('\n\n')
 }
 
-// show_classifier_history history []HistoryEvent
+// show_classifier_history
 fn show_classifier_history(history []HistoryEvent) []string {
 	mut array := ['Classifier History:',
 		'Date & Time (UTC)    Event   From file                            Instances',
