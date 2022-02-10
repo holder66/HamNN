98aca1a0b1e21fb2274dbc810680454ed477b825
Henry Olders <henry@olders.ca>
2022-02-10 17:55:37 -0500

    show_test works for show_classify

--- show.v
+++ show.v
@@ -1,14 +1,8 @@
 // show.v
-
 module hamnn
 
 import etienne_napoleone.chalk
-import math
-
-// show
-pub fn show<T>(a []T) string {
-	return typeof(a).name
-}
+// import math
 
 pub // show_analyze 
 fn show_analyze(result AnalyzeResult) {
@@ -73,62 +67,8 @@ fn show_analyze(result AnalyzeResult) {
 	for key, value in result.class_counts {
 		show << '${key:-20}  ${value:5}'
 	}
-	
 	print_array(show)
 }
-
-pub fn analyze_dataset_old(ds Dataset) []string {
-	cases_count := ds.data[0].len
-	mut show_dataset := ['']
-	mut missing_vals := ds.data.map(missing_values(it))
-	missing_vals << 0
-	mut show_attributes := ['', 'Analysis of Dataset "$ds.path" (File Type ${file_type(ds.path)})',
-		'All Attributes', 'Index  Name                          Count  Uniques  Missing     %   Type',
-		'_____  __________________________  _______  _______  _______  ____   ____']
-	for i, name in ds.attribute_names {
-		show_attributes << '${i:5}  ${name:-27}   ${ds.data[i].len:5}    ${uniques(ds.data[i]):5}     ${missing_vals[i]:4} ${f32(missing_vals[i]) / f32(cases_count) * 100.0:5.1f}   ${ds.inferred_attribute_types[i]}'
-	}
-	show_attributes << '______                             _______           _______ _____'
-	show_attributes << 'Totals (less Class attribute)   ${cases_count * (ds.data.len - 1):10}        ${array_sum<int>(missing_vals):10}  ${f32(array_sum<int>(missing_vals)) / (cases_count * (ds.data.len - 1)) * 100.0:5.2f}%'
-	mut show_types := ['', 'Counts of Attributes by Type', 'Type        Count', '____        _____']
-	for key, value in string_element_counts(ds.inferred_attribute_types) {
-		show_types << '$key          ${value:6}'
-	}
-	show_types << 'Total:     ${ds.inferred_attribute_types.len:6}'
-
-	mut show_discrete_attributes := ['', 'Discrete Attributes for Training',
-		' Index  Name                           Uniques',
-		' _____  __________________________     _______']
-	for key, value in ds.useful_discrete_attributes {
-		show_discrete_attributes << '${key:6}  ${ds.attribute_names[key]:-27}      ${uniques(value):5}'
-	}
-
-	mut show_continuous_attributes := ['', 'Continuous Attributes for Training',
-		' Index  Name                           Min         Max',
-		' _____  __________________________  ______      ______']
-	mut min := 0.0
-	for key, value in ds.useful_continuous_attributes {
-		// to calculate the minimum, strip out missing values (placeholder is -math.max_f32)
-		min = f32_abs(array_min(value.filter(it != -math.max_f32)))
-
-		show_continuous_attributes << '${key:6}  ${ds.attribute_names[key]:-27} ${min:6.3g}      ${array_max(value):6}'
-	}
-
-	mut show_class := ['', 'The Class Attribute: "$ds.Class.class_name"',
-		'Class Value           Cases', '____________________  _____']
-	for key, value in ds.Class.class_counts {
-		show_class << '${key:-20}  ${value:5}'
-	}
-
-	show_dataset << show_attributes
-	show_dataset << show_types
-	show_dataset << show_discrete_attributes
-	show_dataset << show_continuous_attributes
-	show_dataset << show_class
-
-	return show_dataset
-}
-
 
 // show_classifier outputs to the console information about a classifier
 pub fn show_classifier(cl Classifier) {
--- show_test.v
+++ show_test.v
@@ -3,72 +3,19 @@ module hamnn
 
 // import os
 
-// test_show
-fn test_show() {
-	mut opts := Options{
-		show_flag: true
-	}
-	mut ds := load_file('datasets/iris.tab')
-	assert show(analyze_dataset(ds)) == '[]string'
-	mut cl := make_classifier(ds, opts)
-	mut s := typeof(cl).name
-	println(typeof(s).name)
-	// assert show(cl) == ''
-}
-
 fn test_show_analyze() {
-	show_analyze(load_file('datasets/iris.tab'))
-	show_analyze(load_file('datasets/anneal.tab'))
-	// assert pr[2..3] == [
-	// 	'Analysis of Dataset "datasets/developer.tab" (File Type orange_newer)',
-	// ]
+	mut opts := Options{
+		show_flag: false
+	}
+	mut ar := AnalyzeResult{}
 
-	// assert pr[9..11] == [
-	// 	'    3  gender                           13        3        0   0.0   c',
-	// 	'    4  height                           13        9        2  15.4   C',
-	// ]
+	ar = analyze_dataset(load_file('datasets/developer.tab'), opts)
+	show_analyze(ar)
 
-	// assert pr[(pr.len - 6)..] == [
-	// 	'The Class Attribute: "gender"',
-	// 	'Class Value           Cases',
-	// 	'____________________  _____',
-	// 	'm                         8',
-	// 	'f                         3',
-	// 	'X                         2',
-	// ]
-
-	// assert pr[21..24] == [
-	// 	'____        _____',
-	// 	'i               1',
-	// 	'D               3',
-	// ]
-
-	// assert pr[31..34] == [
-	// 	'     1  lastname                             7',
-	// 	'     6  SEC                                  5',
-	// 	'     7  city                                 5',
-	// ]
-
-	ds = load_file('datasets/iris.tab')
-	pr = analyze_dataset(ds, opts)
-
-	// assert pr[28..32] == [
-	// 	'     0  sepal length                 4.300       7.900',
-	// 	'     1  sepal width                      2       4.400',
-	// 	'     2  petal length                     1       6.900',
-	// 	'     3  petal width                  0.100       2.500',
-	// ]
-
-	// ds = load_file('datasets/anneal.tab')
-	// pr = analyze_dataset(ds)
-
-	// assert pr[8..12] == [
-	// 	'    2  steel                           898        8       86   9.6   D',
-	// 	'    3  carbon                          898       10        0   0.0   C',
-	// 	'    4  hardness                        898        7        0   0.0   C',
-	// 	'    5  temper_rolling                  898        2      761  84.7   D',
-	// ]
-}
+	ar = analyze_dataset(load_file('datasets/iris.tab'), opts)
+	show_analyze(ar)
+	
+}	
 
 // test_show_append
 fn test_show_append() ? {
@@ -89,7 +36,6 @@ fn test_show_classifier() {
 		show_flag: true
 	}
 	mut ds := load_file('datasets/iris.tab')
-	assert show(analyze_dataset(ds)) == '[]string'
 	mut cl := make_classifier(ds, opts)
 }
 
