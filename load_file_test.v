0bfe8bb700fb6b5a6405fbae57c00a8e70f2b6ce
Henry Olders <henry@olders.ca>
2022-02-26 08:06:31 -0500

    added more arff datasets and tests to load_file_test.v

--- load_file_test.v
+++ load_file_test.v
@@ -60,6 +60,23 @@ fn test_load_file() {
 	// test that header lines get padded out
 	ds = load_file('datasets/wine.tab')
 	assert ds.attribute_flags == ['class', '', '', '', '', '', '', '', '', '', '', '', '', '']
+
+	// test arff files
+	ds = load_file('datasets/contact-lenses.arff')
+	assert ds.attribute_names == ['age', 'spectacle-prescrip', 'astigmatism', 'tear-prod-rate',
+		'contact-lenses']
+	assert ds.attribute_flags == ['young, pre-presbyopic, presbyopic', 'myope, hypermetrope',
+		'no, yes', 'reduced, normal', 'soft, hard, none']
+	assert ds.attribute_types == ['string', 'string', 'string', 'string', 'string']
+	assert ds.inferred_attribute_types == ['D', 'D', 'D', 'D', 'c']
+	assert ds.useful_continuous_attributes == {}
+
+	ds = load_file('datasets/UCI/iris.arff')
+	assert ds.attribute_names == ['sepallength', 'sepalwidth', 'petallength', 'petalwidth', 'class']
+	assert ds.attribute_flags == ['', '', '', '', 'iris-setosa,iris-versicolor,iris-virginica']
+	assert ds.attribute_types == ['real', 'real', 'real', 'real', 'string']
+	assert ds.inferred_attribute_types == ['C', 'C', 'C', 'C', 'c']
+	assert ds.useful_discrete_attributes == {}
 }
 
 // test_load_classifier_file
