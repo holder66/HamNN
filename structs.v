377f09a99520087eec91b2c2e3ef392a9acd8a2f
Henry Olders <henry@olders.ca>
2022-06-22 16:11:08 -0400

    added 'NA" to the list of "missing data" examples

--- structs.v
+++ structs.v
@@ -4,7 +4,7 @@ module hamnn
 import time
 
 const (
-	missings                   = ['?', '']
+	missings                   = ['?', '', 'NA']
 
 	integer_range_for_discrete = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 )
